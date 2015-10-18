152bb8b9d0e908eb25db6b71363d567c4aeb573a
tjinjin <tjinjinprogram@gmail.com>
2015-10-18 19:31:54 +0900

    無駄なorを削除

--- ff-isucon/site-cookbooks/deploy/files/default/webapp/ruby/app.rb
+++ ff-isucon/site-cookbooks/deploy/files/default/webapp/ruby/app.rb
@@ -103,8 +103,8 @@ SQL
 
     def is_friend?(another_id)
       user_id = session[:user_id]
-      query = 'SELECT COUNT(1) AS cnt FROM relations WHERE (one = ? AND another = ?) OR (one = ? AND another = ?)'
-      cnt = db.xquery(query, user_id, another_id, another_id, user_id).first[:cnt]
+      query = 'SELECT COUNT(1) AS cnt FROM relations WHERE (one = ? AND another = ?)'
+      cnt = db.xquery(query, user_id, another_id).first[:cnt]
       cnt.to_i > 0 ? true : false
     end
 
